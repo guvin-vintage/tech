7384c3e6609bcdcd5c31f5da6fe04f802bf78848
guvin-vintage <109299594+guvin-vintage@users.noreply.github.com>
2022-08-13 02:15:11 +0700

    aa

--- tech.js
+++ tech.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
     $(".main").hide();
     $(".section").hide();
     $("footer").hide();
+    
 
     $(".container-scan").show(function() {
         setTimeout(function() {
@@ -30,11 +31,88 @@ $(document).ready(function() {
 
         },4500)
         
+
     })
 
+    $(".upcoming-phone").addClass("c4-izmir c4-border-bottom-left c4-image-rotate-right c4-gradient-bottom-right")
+    $("figcaption").addClass("c4-layout-bottom-left");
     
+    
+    var today = new Date();
+
+    var day = today.getDate();
+    var month = (today.getMonth() + 1);
+    var year = today.getFullYear();
+
+    var n = (day + 2 * month + (3 * (month + 1)) / 5 + year + (year / 4)) % 7
+    n = Math.round(n);
+    n = parseInt(n);
+    if (n == 7 || n == 0)
+        n = 'Saturday'
+    if (n == 1)
+        n = 'Sunday'
+    else if (n == 2)
+        n = 'Monday'
+    else if (n == 3)
+        n = 'Tuesday'
+    else if (n == 4)
+        n = 'Wednesday'
+    else if (n == 5)
+        n = 'Thursday'
+    else if (n == 6)
+        n = 'Friday'
 
 
+
+
+    if (month == 1)
+        month = 'January'
+    else if (month == 2)
+        month = 'February'
+    else if (month == 3)
+        month = 'March'
+    else if (month == 4)
+        month = 'April'
+    else if (month == 5)
+        month = 'May'
+    else if (month == 6)
+        month = 'June'
+    else if (month == 7)
+        month = 'July'
+    else if (month == 8)
+        month = 'August'
+    else if (month == 9)
+        month = 'September'
+    else if (month == 10)
+        month = 'October'
+    else if (month == 11)
+        month = 'November'
+    else if (month == 12)
+        month = 'December'
+
+
+    var d1 = document.querySelectorAll('.day');
+
+   
+        var date1 = month + ' ' + day + ' ' + year
+       
+    $(".day").html(date1);
+
+     //Go to top
+     $("#gototop").hide();
+     $(window).scroll(function(){
+         if($(this).scrollTop() >=344 )
+         $("#gototop").show("slow");
+         else
+         $("#gototop").hide("slow");
+ 
+ 
+     })
+     $("#gototop").click(function(){
+        $("html,body").animate({
+            scrollTop:0
+        },2000)
+    })
 })
 
    
